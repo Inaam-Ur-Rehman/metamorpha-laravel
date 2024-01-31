d29018b404659ae2bc63cdb23c31e3b780d447c4
Muhammad Inam Ur Rehman <inaamurrehman343@gmail.com>
2024-01-31 19:12:58 +0500

    Added: the linkssss

--- resources/views/components/filament-fabricator/page-blocks/text-with-logo.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/text-with-logo.blade.php
@@ -1,13 +1,17 @@
 @aware(['page'])
 
 @props(['content', 'image'])
-<div class="px-4 py-4 md:py-8">
-    <div class="grid grid-cols-1 gap-8 mx-auto max-w-8xl md:grid-cols-2">
-        <div class=" !font-v400 prose-base prose-ul:list-disc prose-a:underline underline-offset-2">
+<div class="items-center px-4 py-4 my-16 md:py-8">
+    <div class="grid grid-cols-1 gap-8 mx-auto max-w-8xl lg:grid-cols-3">
+        <div
+            class="lg:col-span-2 !font-v400 prose-base prose-ul:list-disc prose-a:underline underline-offset-2 prose-h2:font-vpc700 
+                prose-h2:text-3xl md:prose-h2:text-6xl prose-h2:uppercase prose-h2:mb-4 
+                prose-h3:text-2xl prose-h3:font-vpc700  prose-h3:uppercase prose-h3:mt-8
+            ">
             {!! $content !!}
         </div>
-        <div class="">
-            <img src="{{ Storage::url($image) }}" alt="Bart" class="w-80" />
+        <div class="self-center justify-self-center">
+            <img src="{{ Storage::url($image) }}" alt="Bart" class="w-full" />
         </div>
     </div>
 </div>
