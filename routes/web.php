b3dab6c3e4754613b5678aa777fcace49a5f64a7
Inaam Ur Rehman <inaamurrehman343@gmail.com>
2024-08-15 18:56:23 +0500

    Added:menu

--- routes/web.php
+++ routes/web.php
@@ -24,16 +24,36 @@ use Mollie\Laravel\Facades\Mollie;
 
 // disable / enable routes from the admin panel
 // get list of routes that are disabled from the admin panel
-$disabledRoutes = \App\Models\DisabledRoute::all()->where('disabled', true);
-$disabledRoutes = $disabledRoutes->map(function ($route) {
-    // remove the first slash from the route if it exists
-    return ltrim($route->route, '/');
-})->toArray();
+$disabledRoutes = \App\Models\Header::where('disabled', true)->get()->pluck('slug')->toArray();
 
-// check if the current route is disabled
-if (in_array(request()->path(), $disabledRoutes)) {
-   return abort(404);
+// check if the routes children has disabled routes
+$childDisabledRoutes = \App\Models\Header::where('has_child', true)->get()->map(function($item) {
+    return $item->children;
+})->map(function($item) {
+    return collect($item)->map(function($child) {
+        return $child['child'];
+    });
+})->map(function($item) {
+    return $item->map(function($child) {
+        return collect($child)->map(function($child) {
+            return $child['disabled'] == true ? $child['slug'] : null;
+        });
+    });
+})->flatten()->filter()->toArray();
+
+$disabledRoutes = array_merge($disabledRoutes, $childDisabledRoutes);
+
+// remove trailing slashes
+$disabledRoutes = array_map(function($item) {
+    return ltrim($item, '/');
+}, $disabledRoutes);
+
+// dd($disabledRoutes);
+
+if(in_array(request()->path(), $disabledRoutes)) {
+    return abort(404);
 }
+
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
