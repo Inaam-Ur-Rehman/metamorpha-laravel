7cf838e8827aa0b193b626daf12dce8c72101707
Muhammad Inam Ur Rehman <inaamurrehman343@gmail.com>
2026-05-13 18:53:45 +0500

    fixed: null

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -4,7 +4,7 @@
 <div
     class="max-w-[350px] !font-vp300 text-base h-full flex flex-col justify-between gap-4 hover:scale-[101%] duration-75 cursor-pointer border-none outline-none focus:outline-none card bg-theme-blue"
 >
-    <img src="{{Storage::url($card['image'])}}" alt="{{$card['heading']}}" class="w-full" />
+    <img src="{{Storage::url($card['image'])}}" alt="{{$card['heading']}}" class="w-full aspect-square object-cover" />
     <div class="flex flex-col gap-2 px-4 pb-4 text-center">
         <h2 class="text-xl font-bold text-theme-yellow !font-v700 uppercase">
             {{$card['heading']}}
